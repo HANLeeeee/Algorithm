5909046c11c0fedaf38280639f0f855c66665364
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2022-08-09 01:50:14 +0900

    [Bronze III] Title: 네 번째 점, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.swift
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.swift
@@ -2,23 +2,15 @@ let input1 = readLine()!.split(separator: " ").map { Int(String($0))! }
 let input2 = readLine()!.split(separator: " ").map { Int(String($0))! }
 let input3 = readLine()!.split(separator: " ").map { Int(String($0))! }
 
-
-var x = 0
-var y = 0
-if input1[0] == input2[0] {
-    x = input3[0]
-} else if input1[0] == input3[0] {
-    x = input2[0]
-} else {
-    x = input1[0]
+var result = [Int]()
+for i in 0...1 {
+    if input1[i] == input2[i] {
+        result.append(input3[i])
+    } else if input1[i] == input3[i] {
+        result.append(input2[i])
+    } else {
+        result.append(input1[i])
+    }
 }
 
-if input1[1] == input2[1] {
-    y = input3[1]
-} else if input1[1] == input3[1] {
-    y = input2[1]
-} else {
-    y = input1[1]
-}
-
-print(x, y)
+print(result[0], result[1])
