0aa2a6a36611221ba8dadec3f465876d73fe1bf9
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2022-08-09 04:35:12 +0900

    [Bronze II] Title: 나머지, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.swift
+++ 백준/Bronze/3052. 나머지/나머지.swift
@@ -1,15 +1,7 @@
-var arr = [Int]()
-var arr2 = [Int]()
+var arr = Set<Int>()
 for _ in 0..<10 {
     let input = Int(readLine()!)!
-    arr.append(input%42)
+    arr.insert(input%42)
 }
 
-
-for i in arr {
-    if !arr2.contains(i) {
-        arr2.append(i)
-    }
-}
-
-print(arr2.count)
+print(arr.count)
