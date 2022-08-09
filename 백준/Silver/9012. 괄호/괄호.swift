957f28ab74a22c583feda19310503fb2f84eecb6
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2022-08-10 01:07:33 +0900

    [Silver IV] Title: 괄호, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.swift
+++ 백준/Silver/9012. 괄호/괄호.swift
@@ -1,42 +1,20 @@
-struct Stack<T> {
-    var elements: [T] = []
-  
-    var count : Int {
-        return elements.count
-    }
-    var isEmpty : Bool {
-        return elements.isEmpty
-    }
-    
-    mutating func pop() -> T? {
-        return elements.popLast()
-    }
-    mutating func push(_ element: T) {
-        elements.append(element)
-    }
-    func top() -> T? {
-        return elements.last
-    }
-}
-
-
-var num = Int(readLine()!)!
+let num = Int(readLine()!)!
 
 for _ in 0..<num {
     let input = Array(readLine()!).map {String($0)}
-    var stack = Stack(elements: [])
-
-    for i in input {
-        if i == "(" {
-            stack.push(i)
+    
+    var count = 0
+    for i in 0..<input.count {
+        if input[i] == "(" {
+            count += 1
         } else {
-            if stack.isEmpty {
-                stack.push(i)
+            if count == 0 {
+                count -= 1
                 break
-            } else {
-                stack.pop()
             }
+            count -= 1
         }
     }
-    print(stack.isEmpty ? "YES" : "NO")
+    
+    print(count == 0 ? "YES" : "NO")
 }
