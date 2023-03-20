c2519aa6cd2de551e0cd8a351bd665ad3a065e80
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-03-20 19:52:04 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.swift
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.swift
@@ -1,11 +1,11 @@
-let input = Int(readLine()!)!
+let n = Int(readLine()!)!
 
 var arr = [Int]()
 arr.append(0)
 arr.append(1)
 
-for i in 2...90 {
+for i in 2..<n+1 {
     arr.append(arr[i-2] + arr[i-1])
 }
 
-print(arr[input])
+print(arr[n])
