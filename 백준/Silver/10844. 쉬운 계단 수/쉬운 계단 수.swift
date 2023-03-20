75cd43d488d91e0cf8d7233e55f169188479e6d9
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-03-20 23:17:02 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.swift
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.swift
@@ -1,29 +1,27 @@
-import Foundation
+let n = Int(readLine()!)!
 
-let num = Int(readLine()!)!
-var dp = Array(repeating: Array(repeating: 0, count: 10), count: 101)
-var sum = 0
-
+var map = Array(repeating: Array(repeating: 0, count: 10), count: 101)
 for i in 0...9 {
-    dp[1][i] = 1
+    map[1][i] = 1
 }
 
-if num > 1 {
-    for i in 2...num {
+if n > 1 {
+    for i in 2...n {
         for j in 0...9 {
             if j == 0 {
-                dp[i][j] = dp[i-1][j+1] % 1000000000
+                map[i][j] = map[i-1][j+1] % 1000000000
             } else if j == 9 {
-                dp[i][j] = dp[i-1][j-1] % 1000000000
+                map[i][j] = map[i-1][j-1] % 1000000000
             } else {
-                 dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000
+                map[i][j] = (map[i-1][j-1] + map[i-1][j+1]) % 1000000000
             }
         }
     }
 }
 
+var sum = 0
 for i in 1...9 {
-    sum = (sum + dp[num][i]) % 1000000000
+    sum = (sum + map[n][i]) % 1000000000
 }
 
-print(sum % 1000000000)
+print(sum)
