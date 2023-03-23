a0f91c507e6167334ed70a3cbd720cf44579b534
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-03-23 22:26:41 +0900

    [Silver III] Title: 이친수, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.swift
+++ 백준/Silver/2193. 이친수/이친수.swift
@@ -1,13 +1,8 @@
-import Foundation
-
 let n = Int(readLine()!)!
 
-var map = Array(repeating: 0, count: 91)
-map[1] = 1
-map[2] = 1
-
-for i in stride(from: 3, through: n, by: 1) {
-    map[i] = map[i-2] + map[i-1]
+var map = [0, 1]
+for i in 2..<n+1 {
+    map.append(map[i-1]+map[i-2])
 }
 
 print(map[n])
