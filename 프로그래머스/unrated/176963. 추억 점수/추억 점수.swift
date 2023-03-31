528ec319ad46b49d14bb941f19bde081a22a738e
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-04-01 00:26:55 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.swift
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.swift
@@ -6,8 +6,8 @@ func solution(_ name:[String], _ yearning:[Int], _ photo:[[String]]) -> [Int] {
     for i in 0..<photo.count {
         var sum = 0
         for j in 0..<photo[i].count {
-            if name.contains(photo[i][j]) {
-                sum += yearning[name.firstIndex(of: photo[i][j])!]
+            if let index = name.firstIndex(of: photo[i][j]) {
+                sum += yearning[index]
             }
         }
         result.append(sum)
