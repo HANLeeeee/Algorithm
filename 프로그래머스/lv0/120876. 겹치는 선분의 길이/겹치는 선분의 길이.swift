deb833b9e207999fe9eb7a61bc515c31a274b8f4
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-04-05 00:19:25 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.07 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.swift
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.swift
@@ -1,27 +1,16 @@
 import Foundation
 
 func solution(_ lines: [[Int]]) -> Int {
-    var overlaps: [(start: Int, end: Int)] = []
-    for i in 0..<lines.count {
-        for j in (i+1)..<lines.count {
-            if lines[i][1] >= lines[j][0] && lines[i][0] <= lines[j][1] {
-                var overlapStart = max(lines[i][0], lines[j][0])
-                var overlapEnd = min(lines[i][1], lines[j][1])
-                var overlapLength = overlapEnd - overlapStart
+    // -100 부터 100 까지 이기 때문에 200 개의 원소를 갖는 빈 배열을 만들어준다.
+    var arr: [Int] = Array(repeating: 0, count: 200)
 
-                for index in (0..<overlaps.count).reversed() {
-                    let (start, end) = overlaps[index]
-                    if overlapStart <= end && overlapEnd >= start {
-                        overlapLength += max(end - overlapEnd, overlapStart - start)
-                        overlapStart = min(overlapStart, start)
-                        overlapEnd = max(overlapEnd, end)
-                        overlaps.remove(at: index)
-                    }
-                }
-                overlaps.append((overlapStart, overlapEnd))
-            }
+    for line in lines {
+        //라인의 시작점부터 끝지점까지 반복
+        for j in line[0]..<line[1] {
+            arr[j+100] += 1
         }
     }
 
-    return overlaps.reduce(0) { $0 + $1.end - $1.start }
+    //배열에서 2번이상 겹치는 곳의 개수를 세어준다.
+    return arr.filter({$0 >= 2}).count
 }
