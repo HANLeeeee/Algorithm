bd2c10db6cc6e1542fadb0e8c6399873811135a4
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-04-05 01:10:28 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.71 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.swift
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.swift
@@ -1,29 +1,24 @@
 import Foundation
 
 func solution(_ wallpaper:[String]) -> [Int] {
-    var paperArr = [[Int]]()
+    
+    var lux = Int.max
+    var luy = Int.max
+    var rdx = Int.min
+    var rdy = Int.min
     
     for i in 0..<wallpaper.count {
         let paper = Array(wallpaper[i]).map {String($0)}
         if paper.contains("#") {
             for j in 0..<paper.count {
                 if paper[j] == "#" {
-                    paperArr.append([i, j])
+                    lux = min(lux, i)
+                    luy = min(luy, j)
+                    rdx = max(rdx, i)
+                    rdy = max(rdy, j)
                 }
             }
         }
     }
-
-    var lux = Int.max
-    var luy = Int.max
-    var rdx = Int.min
-    var rdy = Int.min
-    for i in 0..<paperArr.count {
-        lux = min(lux, paperArr[i][0])
-        luy = min(luy, paperArr[i][1])
-        rdx = max(rdx, paperArr[i][0])
-        rdy = max(rdy, paperArr[i][1])
-    }
-    
     return [lux, luy, rdx+1, rdy+1]
 }
