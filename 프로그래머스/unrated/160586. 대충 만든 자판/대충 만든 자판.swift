4f53527ba7d9e0b7e6ffdaaf4d97f326449b332a
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-04-05 02:30:22 +0900

    [unrated] Title: 대충 만든 자판, Time: 2.53 ms, Memory: 16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func solution(_ keymap:[String], _ targets:[String]) -> [Int] {
+    var result = [Int]()
+    var dic = [String: Int]()
+    
+    for i in 0..<keymap.count {
+        let key = Array(keymap[i]).map {String($0)}
+        for j in 0..<key.count {
+            dic[key[j]] = min(dic[key[j]] ?? 100, j+1)
+        }
+    }
+    
+    for i in 0..<targets.count {
+        var count = 0
+        let target = Array(targets[i]).map {String($0)}
+        for j in 0..<target.count {
+            guard dic[target[j]] != nil else {
+                count = -1
+                break
+            }
+            count += dic[target[j]]!
+        }
+        result.append(count)
+    }
+    
+    return result
+}
