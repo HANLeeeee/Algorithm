27bdd7252cf60306f3dff7b3fc7a671936865cfc
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-05-23 23:02:01 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.27 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
@@ -1,18 +1,13 @@
+
 import Foundation
 
 func solution(_ t:String, _ p:String) -> Int {
     var result = 0
-    let count = p.count
-    let tArr = Array(t)
-    
-    for i in 0..<t.count-count+1 {
-        var sub = String()
-        for j in i..<i+count {
-            sub += String(tArr[j])
-        }
-        if Int(sub)! <= Int(p)! {
-            result += 1
-        }
+
+    for i in 0..<t.count-p.count+1 {
+        let sub = t.dropFirst(i).prefix(p.count)
+        
+        result +=  Int(sub)! - Int(p)! <= 0 ? 1 : 0
     }
     
     return result
