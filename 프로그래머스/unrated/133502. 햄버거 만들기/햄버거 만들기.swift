c373236b04cb32d8a6b5ef90d05a6104de7ac34d
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-06-07 01:36:20 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.07 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.swift
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.swift
@@ -3,16 +3,17 @@ import Foundation
 func solution(_ ingredient:[Int]) -> Int {
     var arr = [Int]()
     var result = 0
-    
+
     for num in ingredient {
-        
         arr.append(num)
-        let suffix = arr.suffix(4)
-        if suffix == [1,2,3,1] {
-            result += 1
-            arr.removeLast(4)
+
+        if arr.count > 3 {
+            if arr[arr.count-4..<arr.count] == [1,2,3,1] {
+                arr.removeLast(4)
+                result += 1
+            }
         }
     }
-    
+
     return result
 }
