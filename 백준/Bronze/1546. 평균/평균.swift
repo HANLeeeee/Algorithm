8af0c503b5e9046c156706beb54785d766c9e2b6
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-01 18:10:30 +0900

    [Bronze I] Title: 평균, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.swift
+++ 백준/Bronze/1546. 평균/평균.swift
@@ -1,16 +1,10 @@
-import Foundation
-
 let input = Int(readLine()!)!
-let score = readLine()!.split(separator: " ").map{ Int($0)!}
-
-var arr = [Double]()
-for i in 0..<input {
-    arr.append(Double(score[i]))
-}
+let arr = readLine()!.split(separator: " ").map{ Double($0)!}
 
 let m = arr.max()!
-for i in 0..<arr.count {
-    arr[i] = Double(arr[i])/Double(m) * 100
+var result: Double = 0
+for i in arr {
+    result += i/m*100
 }
-let sum = arr.reduce(0, {$0+$1})
-print(sum/Double(input))
+
+print(result/Double(input))
