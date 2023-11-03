2ee4e086250f04ead4968a03e2c3267ff0de0567
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-03 15:27:33 +0900

    [level 1] Title: 카드 뭉치, Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.swift
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
-    var cards1 = cards1
-    var cards2 = cards2
+    var cards1 = Array(cards1.reversed())
+    var cards2 = Array(cards2.reversed())
     
     for word in goal {
-        if cards1.first == word {
-            cards1.removeFirst()
-        } else if cards2.first == word {
-            cards2.removeFirst()
+        if cards1.last == word {
+            _ = cards1.popLast()
+        } else if cards2.last == word {
+            _ = cards2.popLast()
         } else {
             return "No"
         }
