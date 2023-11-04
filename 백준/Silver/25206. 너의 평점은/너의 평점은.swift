979a7d41f1340d7ed696fdafa14de8126b8e19d3
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-05 02:12:16 +0900

    [Silver V] Title: 너의 평점은, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.swift
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.swift
@@ -1,17 +1,27 @@
-import Foundation
+var dic = [
+    "A+": 4.5,
+    "A0": 4.0,
+    "B+": 3.5,
+    "B0": 3.0,
+    "C+": 2.5,
+    "C0": 2.0,
+    "D+": 1.5,
+    "D0": 1.0,
+    "F": 0.0
+]
 
-var dic: [String: Float] = ["A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0]
+var a: Double = 0
+var b: Double = 0
 
-var a: Float = 0
-var b: Float = 0
-
-(1...20).forEach { _ in
+for _ in 1...20 {
     let input = readLine()!.split(separator: " ").map{ String($0) }
     
-    if input[2] != "P" {
-        a += Float(input[1])! * dic[input[2]]!
-        b += Float(input[1])!
+    if input[2] == "P" {
+        continue
     }
+    let (grade, score) = (Double(input[1])!, input[2])
+    a += grade * dic[score]!
+    b += grade
 }
 
 print(a/b)
