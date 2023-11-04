afe29f5180cf244a4b72f3cc0b045290b796bd96
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-04 16:13:32 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.swift
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.swift
@@ -1,10 +1,10 @@
 func solution(_ s:String) -> String {
     let arr = s.components(separatedBy: " ").map { String($0) }
     var result = ""
-    
     for word in arr {
         for i in 0..<word.count {
-            result += i == 0 ? Array(word)[i].uppercased() : Array(word)[i].lowercased()
+            let w = word.map { String($0) }
+            result += i == 0 ? w[i].uppercased() : w[i].lowercased()
         }
         result += " "
     }
