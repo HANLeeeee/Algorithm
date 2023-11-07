e2d5a4d0c1b651bb1eea389bf5c0dc7c2d95f940
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-07 18:17:06 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 20 ms, Memory: 79648 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.swift
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.swift
@@ -11,12 +11,7 @@ for _ in 0..<input[0] {
 for _ in 0..<input[0] {
     arr2.append(readLine()!.split(separator: " ").map{Int($0)!})
 }
-var result = Array(repeating: Array(repeating: 0, count: input[1]), count: input[0])
-for i in 0..<input[0] {
-    for j in 0..<input[1] {
-        result[i][j] = arr1[i][j] + arr2[i][j]
-    }
-}
+let result = zip(arr1, arr2).map {zip($0, $1).map { $0 + $1 }}
 for row in result {
     print(row.map {String($0)}.joined(separator: " "))
 }
