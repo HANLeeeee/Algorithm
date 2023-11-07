ba0d8d58ce75f8bb40ad8759379073c7d85d5a3c
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-07 19:03:59 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
@@ -2,18 +2,9 @@ import Foundation
 
 func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
     let arr: [Int] = [6, 6, 5, 4, 3, 2, 1]
-    var count = 0
-    var forget = 0
-    for lotto in lottos {
-        if lotto == 0 {
-            forget += 1
-            continue
-        }
-        for num in win_nums {
-            if lotto == num {
-                count += 1
-            }
-        }
-    }
+    
+    let forget = lottos.filter { $0 == 0 }.count
+    let count = win_nums.filter { lottos.contains($0) }.count
+    
     return [arr[count+forget], arr[count]]
 }
