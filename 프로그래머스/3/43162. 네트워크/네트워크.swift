e464abc8525f92027de84e0c02b292cb50498738
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-09 15:52:33 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -1,17 +1,13 @@
-import Foundation
-
 func solution(_ n:Int, _ computers:[[Int]]) -> Int {
-    
-    
     var count: Int = 0
     var check = Array(repeating: false, count: n)
  
-    func bfs(_ num: Int) {
+    func dfs(_ num: Int) {
         check[num] = true
         
         for i in 0..<n {
             if !check[i] && computers[num][i] == 1 {
-                bfs(i)
+                dfs(i)
             }
         }
     }
@@ -19,7 +15,7 @@ func solution(_ n:Int, _ computers:[[Int]]) -> Int {
     for i in 0..<n {
         if !check[i] {
             count += 1
-            bfs(i)
+            dfs(i)
         }
     }
  
