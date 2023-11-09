15beedcd615c9b95d48919cbd97c60a070f04e3c
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-09 16:46:57 +0900

    [level 3] Title: 단어 변환, Time: 0.05 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.swift
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.swift
@@ -13,23 +13,22 @@ func dfs(_ begin: String, _ target: String, _ words: [String], _ count: Int) {
     if target == begin {
         result = result > count ? count : result
         return
-    } else {
-        for i in 0..<words.count {
-            if !check[i] {
-                var differences = 0
+    }
+    for i in 0..<words.count {
+        if !check[i] {
+            var differences = 0
 
-                for (char1, char2) in zip(begin, words[i]) {
-                    if char1 != char2 {
-                        differences += 1
-                    }
-                    if differences > 1 {
-                        break
-                    }
+            for (char1, char2) in zip(begin, words[i]) {
+                if char1 != char2 {
+                    differences += 1
                 }
-                if differences == 1 {
-                    check[i] = words[i] == target ? false : true
-                    dfs(words[i], target, words, count + 1)
+                if differences > 1 {
+                    break
                 }
+            }
+            if differences == 1 {
+                check[i] = words[i] == target ? false : true
+                dfs(words[i], target, words, count + 1)
             }
         }
     }
