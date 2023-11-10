1933e87d0dacf9160acf263acb2b7ba44a106a23
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-10 17:10:59 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.swift
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.swift
@@ -1,42 +1,34 @@
 import Foundation
 
+var result = Int.max
 var check: [Bool] = []
-var result: [Int] = []
 
 func solution(_ begin: String, _ target: String, _ words: [String]) -> Int {
     check = Array(repeating: false, count: words.count)
-    if !words.contains(target) {
-        return 0
-    }
     dfs(begin, target, words, 0)
-    return result.min() ?? 0
+    return result == Int.max ? 0 : result
 }
 
 func dfs(_ begin: String, _ target: String, _ words: [String], _ count: Int) {
-
-    if begin == target {
-        result.append(count)
+    if target == begin {
+        result = result > count ? count : result
         return
     }
-    
-    for (i, word) in words.enumerated() {
-        var dif = 0
+    for i in 0..<words.count {
         if !check[i] {
-            for (char1, char2) in zip(begin, word) {
+            var differences = 0
+
+            for (char1, char2) in zip(begin, words[i]) {
                 if char1 != char2 {
-                    dif += 1
+                    differences += 1
                 }
-                if dif > 1 {
+                if differences > 1 {
                     break
                 }
             }
-            if dif == 1 {
-                check[i] = true
-                dfs(word, target, words, count + 1)
-                if word == target {
-                    result.append(count+1)
-                }
-                check[i] = false
+            if differences == 1 {
+                check[i] = words[i] == target ? false : true
+                dfs(words[i], target, words, count + 1)
             }
         }
     }
