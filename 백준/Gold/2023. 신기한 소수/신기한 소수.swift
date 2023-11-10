dfefe61c3366abca256c0422280ee729ee96a11c
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-10 19:54:21 +0900

    [Gold V] Title: 신기한 소수, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+let n = Int(readLine()!)!
+func check(n: Int) -> Bool {
+    if n == 1 {
+        return false
+    }
+    if n == 2 {
+        return true
+    }
+    let sqrt = Int(sqrt(Double(n)))
+    for i in 2...(sqrt + 1) {
+        if n % i == 0 {
+            return false
+        }
+    }
+    return true
+}
+
+func back(number: String) {
+    if number.count == n {
+        result.append(number)
+        return
+    }
+    for i in 0...9 {
+        let next = "\(number)\(i)"
+        if check(n: Int(next)!) {
+            back(number: next)
+        }
+    }
+}
+var first = ["2", "3", "5", "7"]
+var result: [String] = []
+for f in first {
+    back(number: f)
+}
+
+print(result.joined(separator: "\n"))
