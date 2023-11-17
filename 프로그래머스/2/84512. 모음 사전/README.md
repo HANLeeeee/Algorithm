ebf6a1d16d6050f4473b4c77fef4ad903ea3a3a1
HANLeeeee <74815957+HANLeeeee@users.noreply.github.com>
2023-11-18 03:07:34 +0900

    [level 2] Title: 모음 사전, Time: 5.14 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 5.36 ms
+메모리: 16.6 MB, 시간: 5.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 2:57:2
+2023년 11월 6일 3:7:32
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.swift
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.swift
@@ -1,26 +1,27 @@
 import Foundation
 
 func solution(_ word: String) -> Int {
-    let vowels = ["A", "E", "I", "O", "U"]
-    var dictionary: [String] = []
+    let arr = ["A", "E", "I", "O", "U"]
+    var result: [String] = []
+    var index = 0
     
     func generateWords(_ current: String) {
         if current.count <= 5 {
-            dictionary.append(current)
+            result.append(current)
         } else {
             return
         }
-        
-        for vowel in vowels {
-            generateWords(current + vowel)
+
+        for i in arr {
+            if current + i == word {
+                index = result.count
+                return
+            }
+            generateWords(current + i)
         }
     }
     
     generateWords("")
     
-    if let index = dictionary.firstIndex(of: word) {
-        return index
-    }
-    
-    return -1
+    return index
 }
